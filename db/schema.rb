86f8798f7446516ef77c58a7032802d706a68bec
chrisHembree <111545379+chrisHembree@users.noreply.github.com>
2023-04-22 20:17:01 -0500

    Co-authored-by: Heath Falkenrath <theGeekkit@users.noreply.github.com> Co-authored-by: kylesteinmann <kylesteinmann@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_17_011950) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.datetime "date"
-    t.boolean "status"
+    t.boolean "status", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
